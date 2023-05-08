7219f527adef56b199bb1f5ce52eef952debda01
didostap <dribnucko@gmail.com>
2023-05-08 15:29:03 +0800

    setup deploy

---
+++ script/Deploy.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/Counter.sol";
+
+contract DeployScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Counter feedbackFactory = new Counter();
+        vm.stopBroadcast();
+    }
+}
---
+++ wagmi.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from '@wagmi/cli';
+import { foundry, react } from '@wagmi/cli/plugins';
+
+export default defineConfig({
+  out: '../bali-hackaton/lib/wagmi.hooks.ts',
+  plugins: [
+    foundry({
+      project: '.',
+      include: ['Counter.sol/**'],
+      forge: {
+        build: true,
+      },
+    }),
+    react({
+      useContractEvent: false,
+      useContract: false,
+      useContractItemEvent: false,
+    }),
+  ],
+});
